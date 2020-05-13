03b1bfbde60c6ff888a3e8187c1de1d55db8f153
ChoWonmin <0108257@gmail.com>
2020-05-13 19:58:19 +0900

    state

--- AwesomeProject/components/Picture.js
+++ AwesomeProject/components/Picture.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react';
 import {View, Image} from 'react-native';
 
 export default class Picture extends Component {
+  componentDidMount() {
+    // Toggle the state every second
+    setInterval(
+      () =>
+        this.setState(previousState => ({
+          isShowingText: !previousState.isShowingText,
+        })),
+      1000,
+    );
+  }
+
+  //state object
+  state = {isShowingText: true};
+
   render() {
     let pic = '';
 
@@ -9,6 +23,10 @@ export default class Picture extends Component {
       pic = require('../assets/aladin.jpg');
     } else if (this.props.type === 'two') {
       pic = require('../assets/lat.jpg');
+    }
+
+    if (!this.state.isShowingText) {
+      return null;
     }
 
     return (
