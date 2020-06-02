09f3fa611b65dcf3dfb030808c39852d18254e91
ChoWonmin <0108257@gmail.com>
2020-06-02 22:51:06 +0900

    HashButton Component

--- Navigation/components/HashButton.js
+++ Navigation/components/HashButton.js
@@ -4,9 +4,23 @@ import {View, StyleSheet, Text} from 'react-native';
 export default class HashButton extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello</Text>
+      <View style={styles.hashWarp}>
+        <Text style={styles.hashText}>Hello</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  hashWarp: {
+    height: 36,
+    paddingHorizontal: 12,
+    backgroundColor: '#a6b1e1',
+  },
+  hashText: {
+    lineHeight: 36,
+    color: '#f7f9fe',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
